882f8ae54dfe9a6b1b212e94e4f8ed6c7daab53f
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-12 11:49:17 -0500

    Second commit

---
+++ Mock_BestBuy_API/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mock_BestBuy_API.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
